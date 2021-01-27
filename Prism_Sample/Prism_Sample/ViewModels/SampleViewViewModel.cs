90f81b6f3a93d403dd155e217d776f91f3ee591c
MT-olivia <48093032+MT-olivia@users.noreply.github.com>
2021-01-27 22:51:34 +0900

    ContentControl 用のユーザーコントロールを追加

---
+++ Prism_Sample/Prism_Sample/ViewModels/SampleViewViewModel.cs
@@ -0,0 +1,16 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Prism_Sample.ViewModels
+{
+    public class SampleViewViewModel : BindableBase
+    {
+        public SampleViewViewModel()
+        {
+
+        }
+    }
+}
---
+++ Prism_Sample/Prism_Sample/Views/Contents/SampleView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace Prism_Sample.Views.Contents
+{
+    /// <summary>
+    /// Interaction logic for SampleView
+    /// </summary>
+    public partial class SampleView : UserControl
+    {
+        public SampleView()
+        {
+            InitializeComponent();
+        }
+    }
+}
