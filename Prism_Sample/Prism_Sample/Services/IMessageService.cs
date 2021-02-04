26bcaa3e39b625349be3178bfd9a1a5e7f321d3c
MT-olivia <48093032+MT-olivia@users.noreply.github.com>
2021-02-04 10:34:24 +0900

    MessageService を実装

---
+++ Prism_Sample/Prism_Sample/Services/IMessageService.cs
@@ -0,0 +1,10 @@
+﻿using System.Windows;
+
+namespace Prism_Sample.Services
+{
+    public interface IMessageService
+    {
+        void ShowDialog(string message);
+        MessageBoxResult Question(string message);
+    }
+}
